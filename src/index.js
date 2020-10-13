0928f00a726d42682daad7618edba32146f97115
alex47spb <heap.styx@mail.ru>
2020-10-13 10:48:32 +0300

    init: revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
   
+    var strN = Math.abs(n).toString();
+
+    var reverseStrN = strN.split('').reverse().join('');
+
+    return Number(reverseStrN);
+
 }
